86de9c64c2c3a1a9ea131a068e8f40ff5ce2e180
rafael.diazcruz.19 <rafael.diazcruz.19@cnu.edu>
2024-02-17 22:08:03 -0500

    update

--- experiments/bert_baseline.py
+++ experiments/bert_baseline.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, TFAutoModel
 import tensorflow as tf
 
 MODEL_NAME = 'bert-base-uncased'
-EPOCHS = 100
+EPOCHS = 1
 BATCH_SIZE = 32
 MAX_LENGTH = 512
 LEARNING_RATE = 1e-5
@@ -11,4 +11,4 @@ MODEL_OUT_FILE_NAME = 'bert_baseline_model.h5'
 
 def create_model():
     bert = TFAutoModel.from_pretrained(MODEL_NAME)
-    bert.output_hidden_states = False
+
---
+++ notebooks/json_to_dataset.py
@@ -0,0 +1,63 @@
+import json
+from abc import ABC
+import datasets
+
+
+class EmrqaDataset(datasets.GeneratorBasedBuilder, ABC):
+
+    VERSION = datasets.Version("1.0.0")
+    BUILDER_CONFIGS = [
+        datasets.BuilderConfig(name="emrqa_context", version=VERSION, description="EMRQA dataset with context and no "
+                                                                                  "evidence"),
+        datasets.BuilderConfig(name="emrqa_evidence", version=VERSION, description="EMRQA dataset with only evidence")
+    ]
+
+    DEFAULT_CONFIG_NAME = "emrqa_context"
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            features=datasets.Features(
+                {
+                    "question": datasets.Value("string"),
+                    "context": datasets.Value("string"),
+                    "answers": datasets.Sequence(
+                        {
+                            "text": datasets.Value("string"),
+                            "answer_start": datasets.Value("int32"),
+                            "evidence_start": datasets.Value("int32"),
+                            "evidence": datasets.Value("string"),
+                        }
+                    )
+                }
+            ),
+            supervised_keys=None
+        )
+
+    def _generate_examples(self, filepath):
+        with open('../data/data.json') as f:
+            for dataset in json.load(f)['data']:
+                if dataset['title'] in ['obesity', 'smoking']:
+                    continue
+                for paragraph in dataset['paragraphs']:
+                    context = "".join(paragraph['context'])
+                    for qa_pair in paragraph['qas']:
+                        questions = list(set(qa_pair['question']))
+                        answer = qa_pair['answers'][0]
+                        if answer['text'] == "":
+                            continue
+                        answer_start = answer['answer_start']
+                        answer_start_formatted = answer_start[1] if type(answer_start[1]) != list else answer_start[1][0]
+                        yield {
+                            questions[0],
+                            context,
+                            {
+                                answer['text'],
+                                answer_start_formatted,
+                                answer['evidence'],
+                                answer['evidence_start']
+                            }
+                        }
+
+# train test 80/20 split, 90/10 split for train/val split.
+# do one question from qa_pair to lower the number of samples (2 million to 90k)
+# don't use SQuAD, just use the emrQA dataset
