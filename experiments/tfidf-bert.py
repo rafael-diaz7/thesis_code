fe1a669e71f6f8c70d3342dce215cf49809c97e8
rafael.diazcruz.19 <rafael.diazcruz.19@cnu.edu>
2024-04-10 00:46:36 -0400

    tfidf-bert pipeline ready for training

--- experiments/tfidf-bert.py
+++ experiments/tfidf-bert.py
@@ -1,40 +1,100 @@
+"""
+this script will only be used for data generation for the first step of TF-IDF -> BERT pipeline
+i'm hoping to convert this to the end to end pipeline, but for now, this will only be data generation
+just for ease of using it on dr. henry's computer
+
+TODO: try the clinitokenizer instead of the nltk sentence tokenizer
+^ it's specifically trained on i2b2 data, so it should be significantly better.
+it can be hotswapped with sent_tokenize, so that's nice. install just didn't work for me
+"""
 import pandas as pd
+from transformers import AutoTokenizer
 from nltk.corpus import stopwords
-# import nltk
-# nltk.download('stopwords')
+from nltk.tokenize import sent_tokenize
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
+from gensim.matutils import cossim
 
-# load all data
-# get all contexts
-# make tfidf model using context
-# get the location of the two most interesting terms
-# get the 10 tokens before and after the two most interesting terms and combine to use as evidence
 
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 stopwords = set(stopwords.words('english'))
-df = pd.read_csv('../data/emrqa_context_test.csv')
 
-all_context_texts = df['context'].unique().tolist()
+def create_tfidf_evidence(df, fp):
+    all_context_texts = df['context'].unique().tolist()
 
-# clean stop words from the context
-cleaned_contexts = []
-for context in all_context_texts:
-    cleaned_contexts.append([word for word in context.split() if word.lower() not in stopwords])
-dictionary = Dictionary(cleaned_contexts)
-corpus = [dictionary.doc2bow(context) for context in cleaned_contexts]
-tfidf = TfidfModel(corpus, smartirs='ntc')
+    # clean stop words from the context
+    cleaned_contexts = []
+    for context in all_context_texts:
+        cleaned_contexts.append([word for word in context.split() if word.lower() not in stopwords])
+    dictionary = Dictionary(cleaned_contexts)
+    corpus = [dictionary.doc2bow(context) for context in cleaned_contexts]
+    tfidf = TfidfModel(corpus)
 
-def apply_tfidf_to_context(context):
-    context = [word for word in context.split() if word.lower() not in stopwords]
-    bow = dictionary.doc2bow(context)
-    tfidf_weights = tfidf[bow]
-    tfidf_weights = sorted(tfidf_weights, key=lambda x: x[1], reverse=True)
-    term1, term2 = (dictionary[word[0]] for word in tfidf_weights[:2])
+    df['tf-idf_evidence'] = df.apply(lambda x:
+                                     apply_tfidf_to_context(x['question'], x['context'], dictionary, tfidf),
+                                     axis=1)
+    df = df[['question', 'tf-idf_evidence', 'answer']]
+    df[['start_token', 'end_token']] = df.apply(
+        lambda x: calc_token_spans(x['question'], x['tf-idf_evidence'], x['answer']), axis=1, result_type='expand')
 
-    # get the 10 words before and after the two most interesting terms
-    term1_index = context.index(term1)
-    term2_index = context.index(term2)
-    return " ".join(context[term1_index - 10:term1_index + 11] + context[term2_index - 10:term2_index + 11])
+    df.to_csv(fp, index=False)
 
-df['tf-idf_evidence'] = df['context'].apply(apply_tfidf_to_context)
-df = df[['question', 'tf-idf_evidence', 'answer']]
+
+def apply_tfidf_to_context(question, context, dictionary, tfidf):
+    sentences = sent_tokenize(context)
+    # get vector for each sentence
+    sentence_vectors = [tfidf[dictionary.doc2bow(sentence.split())] for sentence in sentences]
+    question_vector = tfidf[dictionary.doc2bow(question.split())]
+    cosine_similarities = [cossim(question_vector, sentence_vector) for sentence_vector in sentence_vectors]
+    max_index = cosine_similarities.index(max(cosine_similarities))
+    return sentences[max_index]
+
+def calc_token_spans(question, evidence, answer):
+    inputs = tokenizer(question, evidence, return_offsets_mapping=True)
+    offset = inputs['offset_mapping']
+    calc_answer_start = evidence.find(answer)
+    end_char = calc_answer_start + len(answer)
+    sequence_ids = inputs.sequence_ids()
+
+    if calc_answer_start == -1:
+        return 0, 0
+
+    idx = 0
+    while sequence_ids[idx] != 1:
+        idx += 1
+    context_start = idx
+    while sequence_ids[idx] == 1:
+        idx += 1
+    context_end = idx - 1
+
+    idx = context_start
+    try:
+        while idx <= context_end and offset[idx][0] <= calc_answer_start:
+            idx += 1
+        start_position = idx - 1
+
+        idx = context_end
+        while idx >= context_start and offset[idx][1] >= end_char:
+            idx -= 1
+        end_position = idx + 1
+    except:
+        print(offset)
+        print(idx)
+        print(len(offset))
+        print(offset[idx])
+
+    if start_position == end_position:
+        return 0, 0
+    if start_position >= 512 or end_position >= 512:
+        return 0, 0
+    return start_position, end_position
+
+
+if __name__ == '__main__':
+    df_train = pd.read_csv('../data/emrqa_context_train.csv')
+    df_val = pd.read_csv('../data/emrqa_context_val.csv')
+    df_test = pd.read_csv('../data/emrqa_context_test.csv')
+
+    create_tfidf_evidence(df_train, '../data/emrqa_tfidf_evidence_train.csv')
+    create_tfidf_evidence(df_val, '../data/emrqa_tfidf_evidence_val.csv')
+    create_tfidf_evidence(df_test, '../data/emrqa_tfidf_evidence_test.csv')
